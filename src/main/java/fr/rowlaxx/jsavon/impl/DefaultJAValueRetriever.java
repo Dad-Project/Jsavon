d45550ac627cafdf39dd4de68dedd2eb4688848c
RowlaxX <32131492+RowlaxX@users.noreply.github.com>
2021-09-14 10:37:16 +0200

    Implemented DefaultJAValueRetriever

--- src/main/java/fr/rowlaxx/jsavon/impl/DefaultJAValueRetriever.java
+++ src/main/java/fr/rowlaxx/jsavon/impl/DefaultJAValueRetriever.java
@@ -5,6 +5,11 @@ import java.lang.reflect.Field;
 import org.json.JSONArray;
 
 import fr.rowlaxx.jsavon.JSavONArray;
+import fr.rowlaxx.jsavon.annotations.array.JAValue;
+import fr.rowlaxx.jsavon.convert.ConvertRequest;
+import fr.rowlaxx.jsavon.convert.Destination;
+import fr.rowlaxx.jsavon.convert.DestinationResolver;
+import fr.rowlaxx.jsavon.exceptions.JSavONException;
 import fr.rowlaxx.jsavon.interfaces.JAValueRetriever;
 
 public class DefaultJAValueRetriever implements JAValueRetriever {
@@ -14,8 +19,14 @@ public class DefaultJAValueRetriever implements JAValueRetriever {
 	
 	//Méthodes réecrites
 	@Override
-	public <T> T getValue(JSavONArray instance, JSONArray array, Field field) {
-		return null;
+	public <T> T getValue(final JSavONArray instance, final JSONArray array, final Field field) {
+		final JAValue jaValue = field.getAnnotation(JAValue.class);
+		if (jaValue == null)
+			throw new JSavONException("Annotation JAValue must be present for the field " + field);
+		
+		final Destination<T> destination = DestinationResolver.resolve(field, instance);
+		final Object value = array.get(jaValue.index());
+		final ConvertRequest<T> request = new ConvertRequest<>(value, destination);
+		return request.execute();
 	}
-
 }
